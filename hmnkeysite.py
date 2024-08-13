d9ec3f9b76da3f8f9fe731c18a21422d8c8b1f3f
N10ne1337 <98350564+N10ne1337@users.noreply.github.com>
2024-08-13 16:14:19 +0300

    Update hmnkeysite.py

--- hmnkeysite.py
+++ hmnkeysite.py
@@ -1,7 +1,3 @@
-   ''')
-
-if __name__ == '__main__':
-    freezer.freeze()
 from flask import Flask, request, render_template_string
 from flask_frozen import Freezer
 from os import system
@@ -42,33 +38,36 @@ def index():
                 except requests.exceptions.RequestException as e:
                     return f'Ошибка при отправке запроса: {e}'
 
-                return render_template_string('''
-                    <style>
-                        .center {
-                            display: flex;
-                            flex-direction: column;
-                            align-items: center;
-                            justify-content: center;
-                            height: 100vh;
-                        }
-                        form {
-                            display: flex;
-                            flex-direction: column;
-                            align-items: center;
-                        }
-                        input, button {
-                            margin: 5px;
-                        }
-                    </style>
-                    <div class="center">
-                        <form method="post">
-                            <p>Ссылка подтверждения была отправлена на указанную почту. Перейдите по ней чтобы вам пришёл код на почту.</p>
-                            <input type="hidden" name="proxy" value="{{ proxy }}">
-                            <input type="hidden" name="email" value="{{ email }}">
-                            <input type="submit" value="Подтвердить">
-                        </form>
-                    </div>
-                ''', proxy=proxy, email=email)
+                if 'Ваш код выслан на [email protected]' in response.text:
+                    return render_template_string('''
+                        <style>
+                            .center {
+                                display: flex;
+                                flex-direction: column;
+                                align-items: center;
+                                justify-content: center;
+                                height: 100vh;
+                            }
+                            form {
+                                display: flex;
+                                flex-direction: column;
+                                align-items: center;
+                            }
+                            input, button {
+                                margin: 5px;
+                            }
+                        </style>
+                        <div class="center">
+                            <form method="post">
+                                <p>Ссылка подтверждения была отправлена на указанную почту. Перейдите по ней чтобы вам пришёл код на почту.</p>
+                                <input type="hidden" name="proxy" value="{{ proxy }}">
+                                <input type="hidden" name="email" value="{{ email }}">
+                                <input type="submit" value="Подтвердить">
+                            </form>
+                        </div>
+                    ''', proxy=proxy, email=email)
+                else:
+                    return f'Указанная почта не подходит для получения тестового периода. Ответ сервера: {response.text}'
             else:
                 return 'Невозможно получить тестовый период'
         else:
