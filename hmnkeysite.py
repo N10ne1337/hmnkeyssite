00fa41f3f6cc8638e209e37b3c2fd0b5266f29ae
N10ne1337 <98350564+N10ne1337@users.noreply.github.com>
2024-08-14 17:15:42 +0300

    Update hmnkeysite.py

--- hmnkeysite.py
+++ hmnkeysite.py
@@ -13,64 +13,55 @@ def index():
     if request.method == 'POST':
         proxy = request.form.get('proxy')
         email = request.form.get('email')
-        confirm = request.form.get('confirm')
 
         ua = UserAgent()
         headers = {'User-Agent': ua.random}
         proxies = {'https': f'http://{proxy}'} if proxy else None
 
-        if not confirm:
+        try:
+            demo_page = requests.get('https://hidxxx.name/demo/', headers=headers, proxies=proxies)
+            demo_page.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return render_template_string('<div class="container"><div class="alert alert-danger" role="alert">Ошибка доступа к сайту: {{ e }}</div></div>', e=e)
+
+        soup = BeautifulSoup(demo_page.text, 'html.parser')
+        email_input = soup.find('input', {'class': 'input_text_field', 'name': 'demo_mail'})
+
+        if email_input:
             try:
-                demo_page = requests.get('https://hidxxx.name/demo/', headers=headers, proxies=proxies)
-                demo_page.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                return render_template_string('<div class="container"><div class="alert alert-danger" role="alert">Ошибка доступа к сайту: {{ e }}</div></div>', e=e)
-
-            soup = BeautifulSoup(demo_page.text, 'html.parser')
-            email_input = soup.find('input', {'class': 'input_text_field', 'name': 'demo_mail'})
-
-            if email_input:
-                try:
-                    response = requests.post('https://hidxxx.name/demo/success/', data={"demo_mail": email}, headers=headers, proxies=proxies)
-                    response.raise_for_status()
-                except requests.exceptions.RequestException as e:
-                    return render_template_string('<div class="container"><div class="alert alert-danger" role="alert">Ошибка при отправке запроса: {{ e }}</div></div>', e=e)
-
-                soup = BeautifulSoup(response.text, 'html.parser')
-                confirmation_message = soup.find('h2', {'class': 'title'}).get_text(strip=True)
-                
-                # Используем регулярное выражение для проверки текста
-                if re.match(r'^Ваш код выслан\s*на\s*', confirmation_message):
-                    return render_template_string('''
-                        <!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-                            <title>Подтверждение Email</title>
-                          </head>
-                          <body>
-                            <div class="container mt-5">
-                                <div class="alert alert-info" role="alert">
-                                    Ссылка подтверждения была отправлена на указанную почту. Перейдите по ней чтобы вам пришёл код на почту.
-                                </div>
-                                <a href="/" class="btn btn-primary">Домой</a>
-                            </div>
-                          </body>
-                        </html>
-                    ''')
-                else:
-                    return render_template_string('<div class="container"><div class="alert alert-warning" role="alert">Указанная почта не подходит для получения тестового периода. Ответ сервера: {{ response.text }}</div></div>', response=response)
-            else:
-                return render_template_string('<div class="container"><div class="alert alert-warning" role="alert">Невозможно получить тестовый период</div></div>')
-        else:
-            try:
-                response = requests.get(confirm, headers=headers, proxies=proxies)
+                response = requests.post('https://hidxxx.name/demo/success/', data={"demo_mail": email}, headers=headers, proxies=proxies)
                 response.raise_for_status()
-                return render_template_string('<div class="container"><div class="alert alert-success" role="alert">Почта подтверждена. Код отправлен на ваш email.</div></div>')
             except requests.exceptions.RequestException as e:
-                return render_template_string('<div class="container"><div class="alert alert-danger" role="alert">Ошибка при подтверждении: {{ e }}</div></div>', e=e)
+                return render_template_string('<div class="container"><div class="alert alert-danger" role="alert">Ошибка при отправке запроса: {{ e }}</div></div>', e=e)
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+            confirmation_message = soup.find('h2', {'class': 'title'}).get_text(strip=True)
+            
+            # Используем регулярное выражение для проверки текста
+            if re.match(r'^Ваш код выслан\s*на\s*', confirmation_message):
+                return render_template_string('''
+                    <!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+                        <title>Подтверждение Email</title>
+                      </head>
+                      <body>
+                        <div class="container mt-5">
+                            <div class="alert alert-info" role="alert">
+                                Ссылка подтверждения была отправлена на указанную почту. Перейдите по ней чтобы вам пришёл код на почту.
+                            </div>
+                            <a href="/" class="btn btn-primary">Домой</a>
+                        </div>
+                      </body>
+                    </html>
+                ''')
+            else:
+                return render_template_string('<div class="container"><div class="alert alert-warning" role="alert">Указанная почта не подходит для получения тестового периода. Ответ сервера: {{ response.text }}</div></div>', response=response)
+        else:
+            return render_template_string('<div class="container"><div class="alert alert-warning" role="alert">Невозможно получить тестовый период</div></div>')
 
     return render_template_string('''
         <!doctype html>
@@ -79,7 +70,7 @@ def index():
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-            <title>Получить код</title>
+            <title>HideMyName Keys (Beta)</title>
           </head>
           <body>
             <div class="container mt-5">
@@ -87,7 +78,7 @@ def index():
                     <div class="col-md-6">
                         <div class="card">
                             <div class="card-header">
-                                Получить код
+                                <h2>HideMyName Keys (Beta)</h2>
                             </div>
                             <div class="card-body">
                                 <form method="post">
